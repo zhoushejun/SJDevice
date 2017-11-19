28b01b4797213e4989025ca49d2d69b3e6317048
周社军 <965678322@qq.com>
2017-11-20 00:41:19 +0800

    【优化】打印输出当前进程的一些基本信息。

--- SJDevice/UIDevice+SJDevice.m
+++ SJDevice/UIDevice+SJDevice.m
@@ -109,6 +109,37 @@
 }
 /** 获取总内存大小 */
 - (unsigned long long)totalMemorySize {
+    //创建一个NSProcessInfo对象，表示当前进程
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    
+    //获取运行该进程的参数
+    NSArray *arr = [processInfo arguments];
+    NSLog(@"运行该程序的参数为：%@", arr);
+    //获取该进程的进程标示符
+    NSLog(@"该程序的进程标示符(PID)为：%d", [processInfo processIdentifier]);
+    //获取该进程的进程名
+    NSLog(@"该程序的进程名为：%@", [processInfo processName]);
+    //设置该进程的新进程名
+    [processInfo setProcessName:@"custom process name"];
+    NSLog(@"该程序的新进程名为：%@", [processInfo processName]);
+    
+    //获取运行该进程的系统的环境变量
+    NSLog(@"运行该进程的系统的所有环境变量为：%@", [processInfo environment]);
+    //获取运行该进程的主机名
+    NSLog(@"运行该进程的主机名为：%@", [processInfo hostName]);
+    //获取运行该进程的操作系统的版本
+    NSLog(@"运行该进程所在的操作系统名为：%@", [processInfo operatingSystemVersionString]);
+    //获取运行该进程的操作系统的版本
+    NSLog(@"运行该进程所在的操作系统的版本为：%@", [processInfo operatingSystemVersionString]);
+    
+    //获取运行该进程的系统的物理内存
+    NSLog(@"运行该进程的系统的物理内存为：%lld GB", [processInfo physicalMemory]/1024/1024);
+    //获取运行该进程的系统的处理器数量
+    NSLog(@"运行该进程的系统的处理器数量为：%ld", (unsigned long)[processInfo processorCount]);
+    //获取运行该进程的系统的处于激活状态的处理器数量
+    NSLog(@"运行该进程的系统的处于激活状态的处理器数量为：%ld", (unsigned long)[processInfo activeProcessorCount]);
+    //获取运行该进程的系统已运行的时间
+    NSLog(@"运行该进程的系统的已运行时间为：%f", [processInfo systemUptime]);
     return [NSProcessInfo processInfo].physicalMemory;
 }
 
